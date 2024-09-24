71ed2208a8fceb332b7f0ac7d56fead4c7c36705
UncleHanWei <a0973809807@gmail.com>
2024-09-24 17:29:20 +0800

    fix: bug of cannot find next story, cannot add additional attribute to story object, which will cause service crash

--- controller/story.js
+++ controller/story.js
@@ -2,15 +2,24 @@ const { findStory } = require("../service/database/storyService");
 
 const getStory = async (req, res) => {
   const { storyCode } = req.query;
-  const story = (await findStory({ code: storyCode }))[0];
+  let story = (await findStory({ code: storyCode }))[0];
+  if (!story) {
+    res.send({ status: 404 });
+    return;
+  }
+
+  story = story.toObject();
+
   const divergent = ["PVKFN", "B7GWL", "RM4XR", "23F3H"];
   story["divergent"] = false;
   if (divergent.includes(storyCode)) {
     story["divergent"] = true;
     for (let i = 0; i < story["next"].length; i += 1) {
-      (story["nextName"] ??= []).push(storyFile[story["next"][i]]["name"]);
+      const nextStory = (await findStory({ code: story["next"][i] }))[0];
+      (story["nextName"] ??= []).push(nextStory["name"]);
     }
   }
+
   res.send(story);
 }
 
