927682dedfc5de653cd7d702ff20c2ee5c209504
UncleHanWei <a0973809807@gmail.com>
2024-09-25 14:52:29 +0800

    remove console.log in game api

--- controller/game.js
+++ controller/game.js
@@ -1,14 +1,11 @@
 const helloworld = (req, res) => {
   const formData = req.body;
-  console.log(formData);
-
   const ans = ["72", "101", "108", "108", "111", "87", "111", "114", "108", "100"];
   if (formData["answer"].join(",") === ans.join(",")) {
     res.redirect('/signUp');
     return;
   }
   res.send({ message: "wrong answer" })
-
 }
 
 const pascal = (req, res) => {
