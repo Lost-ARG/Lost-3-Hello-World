9559c4c2405f0f3b0d09ea0a9a6a06c5244e8d88
UncleHanWei <a0973809807@gmail.com>
2024-10-23 14:59:48 +0800

    make search input listening Enter press event

--- public/javascripts/backstage/teamPayment.js
+++ public/javascripts/backstage/teamPayment.js
@@ -1,6 +1,15 @@
 const searchResult = document.querySelector("#searchResult");
 const input = document.querySelector("input");
 
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("searchBtn").click();
+  }
+});
 
 const renderSearchResult = (data) => {
   if(data.length === 0) {
